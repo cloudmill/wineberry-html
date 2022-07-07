6be8d61d0c6a79049816ef02b8d4fb79dd064c87
Nikita <nikita.ableev@mail.ru>
2022-07-07 19:41:01 +0300

    Fixes

--- src/app.js
+++ src/app.js
@@ -16,3 +16,49 @@ import "./scripts/real-tabs";
 import "./scripts/catalog-filters";
 import "./scripts/select";
 
+
+$(() => {
+  const tickers = document.querySelectorAll('[data-ticker]')
+  if (tickers.length) {
+    tickers.forEach(item => {
+      const line = item.querySelector('[data-ticker-line]')
+
+      if (line.clientWidth < window.innerWidth) {
+
+        let items = line.querySelectorAll('[data-ticker-item]')
+        // let i = 0;
+        while (line.clientWidth < window.innerWidth) {
+          items.forEach(item => {
+            line.append(item.cloneNode(true))
+          })
+        }
+
+        for (let i = 0; i < 2; i++) {
+          item.append(line.cloneNode(true))
+        }
+        // while (line.clientWidth < window.innerWidth) {
+        //   line.append(items[i].cloneNode(true))
+        //   i++
+        // }
+        // for (let k = 0; k < 2; k++) {
+        //   const lines = item.querySelectorAll('[data-ticker-line]')
+        //   const clone = lines[k].cloneNode(true)
+        //   const cloneItems = clone.querySelectorAll('[data-ticker-item]')
+  
+        //   for (let j = 0; j < i; j++) {
+        //     const cloneItem = cloneItems[j+i].cloneNode(true)
+        //     cloneItems[j].remove()
+        //     clone.append(cloneItem)
+        //   }
+        //   item.append(clone)
+        // }
+      } else {
+        for (let i = 0; i < 2; i++) {
+          item.append(line.cloneNode(true))
+        }
+      }
+
+      item.classList.add('active')
+    })
+  }
+})
