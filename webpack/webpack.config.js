ebaf67192afa4c732275c5668eca62afc546513f
shoeiedibistl <shoeiedibistl2@gmail.com>
2024-06-21 15:20:06 +0300

    return image minimizer

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -180,16 +180,16 @@ module.exports = (env) => {
         ],
       }),
 
-      // new ImageMinimizerPlugin({
-      //   minimizerOptions: {
-      //     cache: true,
-      //     plugins: [
-      //       ["gifsicle", { interlaced: true }],
-      //       ["mozjpeg", { quality: 70 }],
-      //       ["optipng", { optimizationLevel: 5 }],
-      //     ],
-      //   },
-      // }),
+      new ImageMinimizerPlugin({
+        minimizerOptions: {
+          cache: true,
+          plugins: [
+            ["gifsicle", { interlaced: true }],
+            ["mozjpeg", { quality: 70 }],
+            ["optipng", { optimizationLevel: 5 }],
+          ],
+        },
+      }),
 
       new MiniCssExtractPlugin({
         filename: "[name].css",
