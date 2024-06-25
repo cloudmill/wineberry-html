47a702ec94181e0c8655ac256df72d16f0dde940
Maxim <gr.max.nik@yandex.ru>
2024-06-25 10:08:28 +0300

    update

--- src/scripts/disclaimer.js
+++ src/scripts/disclaimer.js
@@ -3,16 +3,20 @@ document.addEventListener("DOMContentLoaded", () => {
   disclaimer = document.querySelector("[data-disclaimer]");
   closeDiscraimer = disclaimer.querySelector("[data-disclaimer-close]");
 
-  const showDisclaimer = sessionStorage["disclaimerHide"];
+  //todo добавил проверку на существование data-disclaimer
+  if (disclaimer) {
+    const showDisclaimer = sessionStorage["disclaimerHide"];
 
-  if (showDisclaimer && showDisclaimer == "true") {
-    disclaimer.classList.remove("--active");
-    body.classList.remove("body--hidden");
+    if (showDisclaimer && showDisclaimer == "true") {
+      disclaimer.classList.remove("--active");
+      body.classList.remove("body--hidden");
+    }
+
+    closeDiscraimer.onclick = () => {
+      disclaimer.classList.remove("--active");
+      body.classList.remove("body--hidden");
+      sessionStorage["disclaimerHide"] = "true";
+    };
   }
 
-  closeDiscraimer.onclick = () => {
-    disclaimer.classList.remove("--active");
-    body.classList.remove("body--hidden");
-    sessionStorage["disclaimerHide"] = "true";
-  };
 });
